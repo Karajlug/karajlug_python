755844bb3f7d0fabf64fc6f80ad66cdc941c756f
lxsameer <lxsameer@gnu.org>
2011-02-18 11:07:25 +0330

    basic view done

---
+++ views.py
@@ -0,0 +1,29 @@
+# -----------------------------------------------------------------------------
+#    Karajlug.org
+#    Copyright (C) 2010  Karajlug community
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# -----------------------------------------------------------------------------
+
+from django.shortcuts import render_to_response as rr
+from django.template import RequestContext
+
+
+def index(request):
+    """
+    index view of karajlug.org
+    """
+    return rr("index.djhtml",
+              context_instance=RequestContext(request))
