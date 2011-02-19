845b11deb479431525d78ae5cf63a9bfeb3efb2a
Sameer Rahmani <lxsameer@gnu.org>
2011-02-19 17:04:42 +0330

    page url added after reverting

--- page/urls.py
+++ page/urls.py
@@ -19,12 +19,8 @@
 
 from django.conf.urls.defaults import patterns
 
-from news.feeds import LatestNews
-
 
 urlpatterns = patterns('',
-        (r'^feed/$', LatestNews()),
-        (r'^(\d+)/$', 'news.views.news_entry'),
-        (r'^$', 'news.views.index'),
+        (r'^(\w+)/$', 'page.views.show_page'),
 
 )
