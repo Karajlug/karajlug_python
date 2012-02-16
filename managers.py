61c61f5f738947b9c571dfd70c0015a2ae3ad78e
lxsameer <lxsameer@gnu.org>
2012-02-16 12:43:29 +0330

    manager added

---
+++ managers.py
@@ -0,0 +1,28 @@
+# -----------------------------------------------------------------------------
+#    Karajlug.org
+#    Copyright (C) 2010-2012  Karajlug community
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# -----------------------------------------------------------------------------
+
+from django.db import models
+from django.conf import settings
+from django.contrib.sites.models import Site
+
+
+class I18nManager(models.Manager):
+    def get_query_set(self):
+        current_site = Site.objects.get(id=settings.SITE_ID)
+        return super(I18nManager, self).get_query_set().filter(site=current_site)
