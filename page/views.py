acd92567177e932c60fdcd70571b59a35fed195a
lxsameer <lxsameer@gnu.org>
2012-02-18 21:59:13 +0330

    rollback

--- page/views.py
+++ page/views.py
@@ -30,13 +30,13 @@ def show_page(request, slug):
     show the page with the given slug
     """
     try:
-        page = Page.objects.filter(slug=slug,
-                                   publish=True)
+        page = Page.objects.get(slug=slug,
+                                publish=True)
 
     except Page.DoesNotExist:
         raise Http404()
 
     return rr("page.html",
-              {"page": page[0],
+              {"page": page,
                "title": "%s | %s" % (_("Karajlug"), page.title)},
               context_instance=RequestContext(request))
