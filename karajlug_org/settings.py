56ce125969095c26ad2ad958b942eadaa076837a
Sameer Rahmani <lxsameer@gnu.org>
2012-09-11 15:12:18 +0430

    bidi css improved

--- karajlug_org/settings.py
+++ karajlug_org/settings.py
@@ -59,7 +59,7 @@ TIME_ZONE = 'Asia/Tehran'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en'
+LANGUAGE_CODE = 'fa'
 
 _ = lambda s: s
 
